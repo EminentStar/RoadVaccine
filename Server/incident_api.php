b2e40d04d5e0b2e5b45591a99e53af6265ecc6bc
eminentstar <junk3843@naver.com>
2016-05-16 16:14:01 +0900

    modified incident_api.php

--- Server/incident_api.php
+++ Server/incident_api.php
@@ -1,46 +1,42 @@
-	<?php
-		define("INCIDENT_API_URL","http://data.ex.co.kr/openapi/safeDriving/safeDrivingSupport?key=5460526240&type=json&incTpCd=00");
-		define("CONSTRUCTION_API_URL","http://data.ex.co.kr/openapi/safeDriving/safeDrivingSupport?key=5460526240&type=json&incTpCd=03");	
-		
-		define("INCIDENT_ID","00");
-		define("CONSTRUCT_ID","03");
-		
-		define("HOST","en605.woobi.co.kr");
-		define("USER","en605");	
-		define("PASSWD","1230");	
-		define("DB_NAME","en605");	
-		
-		define("TIME_QUANTUM",60);
+<?php
+/* 한국도로공사의 교통데이터 API의 주소를 저장한다. */
+	define("INCIDENT_API_URL","http://data.ex.co.kr/openapi/safeDriving/safeDrivingSupport?key=5460526240&type=json&incTpCd=00");
+	define("CONSTRUCTION_API_URL","http://data.ex.co.kr/openapi/safeDriving/safeDrivingSupport?key=5460526240&type=json&incTpCd=03");	
+	
+	//API의ID번호가 00이면 사고, 03이면 공사정보임을 나타낸다.
+	define("INCIDENT_ID","00");
+	define("CONSTRUCT_ID","03");
+	
+	//DB 접속정보를 저장한다.
+	define("HOST","en605.woobi.co.kr");
+	define("USER","en605");	
+	define("PASSWD","1230");	
+	define("DB_NAME","en605");	
+	
+	define("TIME_QUANTUM",60);
 
-		define("ANALDTMC","2");
-		define("SEND_ONETIME",40);	
+	define("ANALDTMC","2");
+	define("SEND_ONETIME",40);	
 
-		require_once("gcm_message.php");
-		require_once("Incident.php");
-		require_once("Construction.php");	
-		require_once("IncidentServerService.php");
-			
+	require_once("gcm_message.php");
+	require_once("Incident.php");
+	require_once("Construction.php");	
+	require_once("IncidentServerService.php");
 
-		$serverMethods = new IncidentServerService;
-		
-	//	$incidentUrl = API_URL.INCIDENT_ID;
-	//	$constructUrl = API_URL.CONSTRUCT_ID;	
+	$serverMethods = new IncidentServerService;
 
-		$isChanged = false;
+	$isChanged = false;
 
-		/* The variable $incident is going to be used for store the last incident of incident table in DB  */	
-		$incident = new Incident;	
-		$construction = new Construction;
+	/* The variable $incident is going to be used for store the last incident of incident table in DB  */	
+	$incident = new Incident;	
+	$construction = new Construction;
 
 	$serverMethods->getLastIncident($incident);
 	$serverMethods->getLastConstruction($construction);
 
-	/*Testing variable */
-	$testCnt = 0;
-
-	/* infinite check Incidents and Construction API loop */
+	// 공사/사고 API를 계속적으로 체크하는 루프
 	while(true){
-		/*Request The Incident API and get the information as the JSON format*/
+		//공사/사고 API를 요청하고 JSON형식으로 변환한다.
 		$incResult = $serverMethods->getApiResult(INCIDENT_API_URL);
 		try{
 			$incResult_array = json_decode($incResult);
@@ -55,7 +51,7 @@
 			echo 'Caught exception: ', $e->getMessage(), "\n";
 		}	
 
-		/* Seperate JSON Array to the each JSON Object  */
+		//JSON array로부터 각 JSON object를 뽑아낸다.
 		$incList = $incResult_array->{"list"};
 		$incCount = $incResult_array->{"count"};
 		$incCode = $incResult_array->{"code"};
@@ -64,49 +60,34 @@
 		$consCount = $consResult_array->{"count"};
 		$consCode = $consResult_array->{"code"};
 		
-		/* get the last index of the incident list  */
+		//공사/사고의 마지막 index를 구한다.
 		$incCntNum = $incCount - 1;
 		$consCntNum = $consCount - 1;
 	
-		/* get the last incident from the incident array which we got from API Server */
-		//$incLastIndex = $incList[$incCntNum];
-		//$consLastIndex = $consList[$consCntNum];		
-		
 		//Temporary arrays.
 		$incArray = array();
 		$consArray = array();
 	
-		//print('api to JSON');
+		//JSON 형태의 사건 API를 특정 포맷의 Array로 변경한다.
 		$serverMethods->apiJsonToArray($incCount, $incList, $incArray);
 		$serverMethods->apiJsonToArray($consCount, $consList, $consArray);
 	
-		//print('remove same element(inc)\n');	
+		//사건에서 중복 사건들을 제거한다.
 		$serverMethods->fastRemoveSameElement($incArray);
-
-		//print('remove same element(cons)\n');	
 		$serverMethods->fastRemoveSameElement($consArray);
 
 		$incArrayCnt = count($incArray);
 		$consArrayCnt = count($consArray);		
 
-		//print(count($incArray));
-		//print(count($consArray));
-
 		$LastIncArrIndex = $incArray[$incArrayCnt - 1];
 		$LastConsArrIndex = $consArray[$consArrayCnt - 1];
 
-		//Create the array which is going to be basis of the entire information string.
+		//전체 정보 문자열의 기본이 될 $wholeArray 어레이를 생성한다.
 		$wholeArray = array();
 		$wholeStr = "";	
-		/*
-		if($incLastIndex->{"analDtmc"} == NULL){
-			$lastIncIndexAnaldtmc = "xx";
-		}else{
-			$lastIncIndexAnaldtmc = $incLastIndex->{"analDtmc"};
-		}			
-		*/
 
 		/* Compare whether the last incident of Table is the same as the last incident element of array */
+		//사고 현황의 변화가 있을때
 		if( ($LastIncArrIndex[ANALDTMC] != $incident->getAnaldtmc() ) || ($incArrayCnt != $incident->getCount()) )
 		{
 			//Not the same, So you gotta put the last incident element to the Out table.	
@@ -118,10 +99,9 @@
 			$serverMethods->insertIncident($incident);
 
 			/* The status has changed. */
-			if($incArrayCnt > 0){
-				//$serverMethods->apiJsonToArray($incCount, $incList, $wholeArray);
+			if($incArrayCnt > 0)
 				$wholeArray = array_merge($wholeArray, $incArray);
-			}
+			
 			$isChanged = true;
 		}else{
 			echo "The LAST accident has NOT changed.\n";
@@ -129,8 +109,8 @@
 			
 	    	$consAnaldtmc = $construction->getAnaldtmc();
 		$consCnt = $construction->getCount();
-	
-		/*  Check the Consctruction state   */	
+		
+		//공사 현황의 변화가 있을때
 		if(($LastConsArrIndex[ANALDTMC] != $construction->getAnaldtmc()) || ($consArrayCnt != $construction->getCount()) ){
 			echo "The construction status HAS changed. \n";
 					
@@ -139,16 +119,13 @@
 
 			$serverMethods->insertConstruction($construction);
 			
-			if( ($isChanged == false) && ($incArrCnt > 0) ){
-				//$serverMethods->apiJsonToArray($incCount, $incList, $wholeArray); //you need to give the same status to the Drivers.
+			if( ($isChanged == false) && ($incArrCnt > 0) )
 				$wholeArray = array_merge($wholeArray, $incArray);
-			}
 
 			/* The status has changed. */
-			if($consArrayCnt > 0){
-				//$serverMethods->apiJsonToArray($consCount, $consList, $wholeArray);
+			if($consArrayCnt > 0)
 				$wholeArray = array_merge($wholeArray, $consArray);
-			}
+			
 			$isChanged = true;
 
 		}else{
@@ -166,7 +143,7 @@
 		//$subArray is array of the subarrays separated from $wholeArray
 		$subArray = array_chunk($wholeArray, SEND_ONETIME );
 		
-		//Push the Result String to Android Application by using GCM
+		//결과 문자열을 GCM을 이용하여 안드로이드 애플리케이션으로 푸시한다.
 		print(" wholeCnt is $wholeCnt \n");
 		print("quotient is $quotient \n");	
 		if($isChanged == true){
@@ -182,9 +159,9 @@
 			}else{	
 				print("quotient variable is not 0 \n");
 				for($i = 0; $i <= $quotient ; $i++){
-					if( ($i == $quotient) && ($remainder == 0) ){
+					if( ($i == $quotient) && ($remainder == 0) )
 						break;
-					}
+					
 					$wholeStr = $serverMethods->multipleArrayToString($subArray[$i]);
 					$wholeStr = "{".$wholeStr."}";		
 
@@ -192,9 +169,9 @@
 					$gcmMessage = new gcm_message;
 					$gcmMessage->sendMessage($wholeStr);
 
-					if( ($i == $quotient) && ($remainder == 0) ){
+					if( ($i == $quotient) && ($remainder == 0) )
 						break;
-					}
+					
 					for($sec = 0 ; $sec < 5 ; $sec++){
 						echo ".";
 						sleep(1);
